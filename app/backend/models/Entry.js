24788b9deec6fa9033b2a77f8918fd2cd58429dd
Yip <crystal.yip@cgi.com>
2019-02-16 20:00:09 -0500

    updates

--- app/backend/models/Entry.js
+++ app/backend/models/Entry.js
@@ -15,4 +15,4 @@ let Entry = new Schema({
     },
 
 });
-export default mongoose.model('Entry', Entry, 'entries');
+export default mongoose.model('Entry', Entry, 'test');
--- app/backend/server.js
+++ app/backend/server.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import Entry from './models/Entry';
 const app = express();
 const router = express.Router();
+const upload = require('./upload');
+
 app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/test');
@@ -22,6 +24,10 @@ router.route('/entries').get((req, res) => {
             res.json(entries);
     });
 });
+
+app.post('/upload', upload);
+
+
 
 app.use('/', router);
 app.listen(4000, () => console.log(`Express server running on port 4000`));
